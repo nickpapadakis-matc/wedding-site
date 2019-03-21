edd9becb6d97a5d40da5f68bf66939707b136235
nickamadeus <papadakn@gmatc.matc.edu>
2019-03-21 18:37:44 -0500

    basic backend functionality is done, user can rsvp and information is stored in our database

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//create schema
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  coming: {
+    type: Boolean,
+    required: true
+  },
+  email: {
+    type: String
+  },
+  food: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+//Load user model
+const User = require('../../models/User');
+
+//  @route  GET api/users/test
+//  @desc   Tests user route
+//  @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
+
+//  @route  GET api/users/rsvp
+//  @desc   submits user details to db
+//  @access Public
+router.post('/rsvp', (req, res) => {
+  User.findOne({ name: req.body.name }).then(user => {
+    if (user) {
+      return res.status(400).json({ name: 'Name already exists' });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        coming: req.body.coming,
+        email: req.body.email,
+        food: req.body.food
+      });
+
+      newUser
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
+    }
+  });
+});
 
 module.exports = router;
